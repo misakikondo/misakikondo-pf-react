4443914ba5e1e125cd9b1debe4fbcd8cb0a5c178
misakikondo <misakikondo718@gmail.com>
2020-12-07 17:25:36 +0900

    updated about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,31 @@ import "./about.css";
 export default function About() {
   return (
     <div>
-      
+      <h1 className='sub-title'>about</h1>
+      <div className='about-container'>
+        <div className='my-image'>
+          {/* <img src='images/me-min.jpg' className='me-img'> */}
+        </div>
+        <div className='explain'>
+          <h2 className='explain-container'>近藤 未咲 1991年生まれ（29歳)<br/>
+          愛知県 名古屋市出身 東京都在住<br/>
+          龍谷大学 文学部 日本語日本文学科 2015年卒<br/>
+          テックキャンプ 丸の内校 2020年卒<br/>
+          スノーボードグッズの販売員を経験後、ワーキングホリデーを利用し、<br/>
+          カナダ・バンフ国立公園内のカフェで就業。<br/>
+          その後、営業事務を経験し、30歳になる直前で将来を考えたときに、<br/>
+          これから進化し続けていくI Tの分野に携わり、自分のアイデアを活かせる仕事がしたい<br/>
+          と思うようになり、テックキャンプでプログラミングを学ぶ。<br/>
+          現在は転職活動をしながら自走力を活かし、JavaScript・React.jsを学習中。<br/>
+          <br/>
+          趣味：サーフィン・スノーボード・アクセサリー作り・動画編集<br/>
+          <br/>
+          関連リンク
+          <a href="https://www.wantedly.com/users/143261148?profile_v1=true" target="_blank" rel="noopener noreferrer">Wantedly</a>/
+          <a href="https://github.com/misakikondo" target="_blank" rel="noopener noreferrer">Github</a>
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
