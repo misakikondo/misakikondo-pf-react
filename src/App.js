0cf7f07eaad26bedf3bd5589ed357a7290b321cc
misakikondo <misakikondo718@gmail.com>
2020-12-07 16:21:17 +0900

    created footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Menu from "./components/menu/Menu.jsx";
 // import About from "./components/about/About.jsx";
 // import Skills from "./components/skills/Skills.jsx";
 // import Contacts from "./components/contacts/Contacts.jsx";
-// import Footer from "./components/footer/Footer.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
   return (
@@ -14,15 +14,19 @@ function App() {
           <h1 className='title-font'>Misaki Kondo</h1> 
         </div>
         <div className='header-right'>
-          <Menu/>
-        </div>
+          <Menu/></div>
       </header>
-{/* 
-      <main>
-        
-      </main>
 
-      <Footer/> */}
+      {/* <main>
+        <div class='common-container' id='private-works-id'><Works/></div>
+        <div class='common-container' id='private-works-id'><About/></div>
+        <div class='common-container' id='private-works-id'><Skills/></div>
+        <div class='common-container' id='private-works-id'><Contacts/></div>
+      </main> */}
+
+      <footer>
+        <Footer/>
+      </footer>
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <div>
+      <div className='footer-container'>
+        <a href='https://github.com/misakikondo'>
+          <i href='https://github.com/misakikondo' target="_blank" rel="noopener noreferrer"
+          className='fab fa-github fa-3x iconcolor'></i>
+        </a>
+        <a href='https://www.instagram.com/misaki_was_here/'>
+          <i href='https://www.instagram.com/misaki_was_here/' target='_blank' rel='noopener noreferrer' 
+          className='fab fa-instagram fa-3x iconcolor'></i>
+        </a>
+        <a href='https://www.youtube.com/channel/UCZ8ZHxoYV_AnWrKCC6456Tw/'>
+          <i href='https://www.youtube.com/channel/UCZ8ZHxoYV_AnWrKCC6456Tw/' target='_blank' rel='noopener noreferrer' 
+          className='fab fa-youtube fa-3x iconcolor'></i>
+        </a>
+        <a href='https://music.apple.com/jp/playlist/%E3%81%AB%E3%81%BB%E3%82%93%E3%81%98%E3%82%83%E3%81%AA%E3%81%84/pl.u-GgA5kZBcbx7zLA'>
+          <i href='https://music.apple.com/jp/playlist/%E3%81%AB%E3%81%BB%E3%82%93%E3%81%98%E3%82%83%E3%81%AA%E3%81%84/pl.u-GgA5kZBcbx7zLA' target='_blank' rel='noopener noreferrer' 
+          className='fas fa-compact-disc fa-3x iconcolor'></i>
+        </a>
+      </div>
+    </div>
+  );
+}
