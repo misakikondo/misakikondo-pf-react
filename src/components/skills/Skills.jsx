375f411e5e0a11700c583d689c101869fb274b29
misakikondo <misakikondo718@gmail.com>
2020-12-07 17:25:14 +0900

    updated skills component

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,7 +4,10 @@ import "./skills.css";
 export default function Skills() {
   return (
     <div>
-      
+      <h1 className='sub-title'>skills</h1>
+      <h2 className='skills-container'>HTML/CSS/Javascript/Ruby/RubyonRails<br/>
+        Git/AWS(S3)/Heroku<br/>
+      </h2>
     </div>
   );
 }
