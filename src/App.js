4b82f8636c1597a60d5b1551097e8ad6d41e376f
misakikondo <misakikondo718@gmail.com>
2021-01-27 14:50:13 +0900

    updated App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/about/About.jsx";
 import Skills from "./components/skills/Skills.jsx";
 import Contacts from "./components/contacts/Contacts.jsx";
 import Footer from "./components/footer/Footer.jsx";
+import Photo from "./components/photo/Photo.jsx";
 import { animateScroll as scroll } from 'react-scroll';
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
       <main>
         <div className='top-container' id='private-top-id'><Top/></div>
         <div className='common-container' id='private-works-id'><Works/></div>
+        <div className='common-container' id='photo-id'><Photo/></div>
         <div className='common-container' id='private-about-id'><About/></div>
         <div className='common-container' id='private-skills-id'><Skills/></div>
         <div className='common-container' id='private-contacts-id'><Contacts/></div>
