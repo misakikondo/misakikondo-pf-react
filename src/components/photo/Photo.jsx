3c928a9666800498238b64b170982ee40ada3dfb
misakikondo <misakikondo718@gmail.com>
2021-01-27 14:50:05 +0900

    created photo component

---
+++ src/components/photo/Photo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./photo.css";
+import photo1 from "../../images/photo1.png"
+import photo2 from "../../images/photo2.png"
+import photo3 from "../../images/photo3.png"
+import photo4 from "../../images/photo4.png"
+import photo5 from "../../images/photo5.png"
+import photo6 from "../../images/photo6.png"
+
+export default function Photo() {
+  return(
+    <div>
+      <h1 className='sub-title'>photography</h1>
+      <div className='photo-container'>
+        <img alt="" src={photo1} className='photo-img'/>
+        <img alt="" src={photo2} className='photo-img'/>
+        <img alt="" src={photo3} className='photo-img'/>
+        <br/>
+        <img alt="" src={photo4} className='photo-img'/>
+        <img alt="" src={photo5} className='photo-img'/>
+        <img alt="" src={photo6} className='photo-img'/>
+      </div>
+    </div>
+  );
+}
